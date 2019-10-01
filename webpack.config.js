360ce171fc0579d8699e781db91a2a4812484968
BearGuy <stephen@peterkins.ca>
2019-10-01 17:16:06 +0800

    reintroduce module structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,21 @@ const path = require('path');
 // This is main configuration object.
 // Here you write different options and tell Webpack what to do
 module.exports = {
+    target: 'web',
     node: {
         fs: "empty"
     },
 
     // Path to your entry point. From this file Webpack will begin his work
     entry: ["babel-polyfill",'./web/src/index.js'],
-    //entry: './src/index.js',
 
-    // Path and filename of your result bundle.
     // Webpack will bundle all JavaScript into this file
     output: {
         path: path.resolve(__dirname, './web/dist'),
         filename: 'browser.js'
     },
 
-    // need to keep these seperate
-    // externals: {
-    //     net: "net",
-    //     uws: "uws"
-    // },
-
-  // rules that we're governed by
+    // rules that we're governed by
     module: {
         rules: [
             {
